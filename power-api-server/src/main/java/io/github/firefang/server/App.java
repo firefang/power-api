37adcb46d313fbc9449c5e4e70abd4f80412d103
xinufo <1064752091@qq.com>
2019-05-18 08:46:32 +0800

    feat: initialize project structure

---
+++ power-api-server/src/main/java/io/github/firefang/server/App.java
@@ -0,0 +1,17 @@
+package io.github.firefang.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main class
+ *
+ */
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+}
